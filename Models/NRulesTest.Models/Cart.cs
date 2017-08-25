417b81defdd4499303c9c9d5fde8d2828c5f2753
bstauff <brian.stauff.github@gmail.com>
2017-08-25 17:54:46 -0500

    really simple rule. not sure if I did this right

---
+++ Models/NRulesTest.Models/Cart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NRulesTest.Models
+{
+    public class Cart
+    {
+        public double Total { get; set; }
+
+        public IEnumerable<Fruit> FruitsInCart { get; set; }
+
+        public void AddToTotal(double price)
+        {
+            Total += price;
+        }
+    }
+}
---
+++ Rules/NRules.Rules/InSeasonDiscountRule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NRules.Fluent.Dsl;
+using NRulesTest.Models;
+
+namespace NRules.Rules
+{
+    public class InSeasonDiscountRule : Rule
+    {
+        public override void Define()
+        {
+            Fruit fruit = null;
+            Cart cart = null;
+
+            When()
+                .Match<Fruit>(() => fruit, x => x.IsInSeason)
+                .Query(() => cart.FruitsInCart, x => x.Match<Fruit>(a => a.IsInSeason).Collect().Where(a => a.Any()));
+
+            Then()
+                .Do(x => cart.FruitsInCart.ToList().ForEach(a => cart.AddToTotal(a.Price)));
+        }
+    }
+}

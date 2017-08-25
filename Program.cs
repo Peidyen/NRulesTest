f3dcf96183c155522e5c4412c0de0648a11b359b
bstauff <brian.stauff.github@gmail.com>
2017-08-25 18:00:26 -0500

    code to run the rules.  doesn't seem to work right

--- Program.cs
+++ Program.cs
@@ -3,13 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NRules;
+using NRules.Fluent;
+using NRules.Rules;
+using NRulesTest.Models;
 
 namespace NRulesTest
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Console.WriteLine("Starting NRules test...");
+
+            var repository = new RuleRepository();
+            repository.Load(x => x.From(typeof(InSeasonDiscountRule).Assembly));
+
+            var factory = repository.Compile();
+
+            var session = factory.CreateSession();
+
+            var cart = new Cart
+            {
+                Total = 0.0,
+                FruitsInCart = new List<Fruit>
+                {
+                    new Orange{IsInSeason = true, Price = 4.00},
+                    new Orange{IsInSeason = false, Price = 9.00}
+                }
+            };
+
+            session.Insert(cart);
+
+            session.Fire();
+
+            Console.WriteLine("Done.  Hit enter to exit");
+
+            Console.ReadLine();
         }
     }
 }
